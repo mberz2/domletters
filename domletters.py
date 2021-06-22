ca5558ffccbd95def831e00824645caa834bc9bd
Matthew Berzinskas <mberz2@pdx.edu>
2021-06-22 16:38:05 -0700

    Updates for Count

--- domletters.py
+++ domletters.py
@@ -3,10 +3,9 @@ import string
 from collections import Counter as ctr
 import re
 
-print("domletters.py")
-
 alphabet = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ\n')
 words = []
+final = []
 
 def is_ascii(s):
 	return all(c in alphabet for c in s)
@@ -16,8 +15,13 @@ with open(sys.argv[1], "r") as f:
 		words.extend(l.split(" "))
 
 for w in words:
-
-	count = 0
 	w = w.upper()
 	if is_ascii(w):
-		print(ctr(w).most_common(1)[0])
+		final.append(ctr(w).most_common(1)[0])
+		#print(ctr(w).most_common(1)[0])
+
+count = 0
+for key, w in final:
+	count += (w)
+
+print(count)
